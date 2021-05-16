20c0fa28c11bf3ffc93fb0ebb93307a8029d1282
benpoarch <978316@swansea.ac.uk>
2021-05-17 00:38:41 +0100

    First commit

--- app/src/main/java/com/example/moodtracker/MainActivity.kt
+++ app/src/main/java/com/example/moodtracker/MainActivity.kt
@@ -10,6 +10,9 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_mood_test.*
+import ru.cleverpumpkin.calendar.CalendarDate
+import ru.cleverpumpkin.calendar.CalendarView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,11 +37,45 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
+        //for calendar widget
+        val calendarView: CalendarView = findViewById(R.id.calendar_view)
+        val calendar = Calendar.getInstance()
+
+        // Initial date
+        calendar.set(2018, Calendar.JUNE, 1)
+        val initialDate = CalendarDate(calendar.time)
+
+        // Minimum available date
+        calendar.set(2018, Calendar.MAY, 15)
+        val minDate = CalendarDate(calendar.time)
+
+        // Maximum available date
+        calendar.set(2018, Calendar.JULY, 15)
+        val maxDate = CalendarDate(calendar.time)
+
+        // List of preselected dates that will be initially selected
+        //val preselectedDates: List<CalendarDate> = getPreselectedDates()
+
+        // The first day of week
+        val firstDayOfWeek = java.util.Calendar.MONDAY
+
+        // Set up calendar with all available parameters
+        calendarView.setupCalendar(
+            initialDate = initialDate,
+            minDate = minDate,
+            maxDate = maxDate,
+            selectionMode = CalendarView.SelectionMode.NONE,
+            firstDayOfWeek = firstDayOfWeek,
+            showYearSelectionView = true
+            //selectedDates = preselectedDates
+        )
 
 
 
     }
 
+    //for calendar
+    private fun getPreselectedDates() {}
 
 
     //for login activity
